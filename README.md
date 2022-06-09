34b6771eaa5af1e39ae83a385e214099e14eab81
matoruru <40382980+matoruru@users.noreply.github.com>
2022-06-09 12:49:31 +0900

    Merge to main (#1)
    * Added extension
    * Added icon

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# chrome-extensions-auto-2fa
-I don't like clicking "Call me" button every time when the multi-factor screen shows up! This automates it for you.
+# Auto Microsoft 2FA
 
-# Usage
+This automates the clicking-buttons process for the Microsoft 2FA for you.
 
-Under construction ...
+# Installation
+
+1. Clone this repo.
+
+   ```
+   git clone git@github.com:matoruru/chrome-extension_auto-microsoft-2fa.git -b wip-initial
+   ```
+
+1. **Enable** developer mode from `chrome://extensions/`.
+
+1. Click *Load unpacked*.
+
+1. Select `extension` folder.
+
+1. **Disable** developer mode from `chrome://extensions/`.
+
+1. Enjoy your life without clicking Microsoft 2FA 🎉
---
+++ extension/app.js
@@ -0,0 +1,83 @@
+const logLabel = 'matoruru/chrome-extension_auto-microsoft-2fa: '
+
+const appLog = (...args) => console.log(`${logLabel} `, ...args)
+
+const intervalTime = 50
+
+const hasText = (elem, s) => elem.textContent.includes(s)
+
+const pickElement = (elems, s) => elems.filter(elem => hasText(elem, s))[0]
+
+const checkTitle = () => {
+  return new Promise((resolve, reject) => {
+    const timerId = setInterval(() => {
+      const title = document.querySelector('[role=heading]')
+
+      if (!title) return
+
+      clearInterval(timerId)
+
+      if (hasText(title, 'Verify your identity')) {
+        resolve()
+      } else {
+        reject(`Title is not correct (Expected "Verify your identity" but "${title.textContent}"). This migit be another page!`)
+      }
+    }, intervalTime)
+  })
+}
+
+const getButtons = () => {
+  return new Promise((resolve, reject) => {
+    const timerId = setInterval(() => {
+      const buttonsElements = Array.from(document.querySelectorAll('div.table[role=button]'))
+
+      if (buttonsElements.length == 0) return
+
+      const textButton = pickElement(buttonsElements, 'Text +')
+      const callButton = pickElement(buttonsElements, 'Call +')
+
+      clearInterval(timerId)
+
+      if (textButton || callButton) {
+        resolve({ textButton, callButton })
+      } else {
+        reject('Buttons are not existing as expected!')
+      }
+    }, intervalTime)
+  })
+}
+
+const app = async () => {
+  // Check if the title is "Verify your identity".
+  await checkTitle()
+
+  // Expecting the UI that looks like:
+  //
+  // Verify your identity
+  //   Text +XX XXXXXXX92   <--- textButton
+  //   Call +XX XXXXXXX92   <--- callButton
+  //
+  const { textButton, callButton } = await getButtons()
+
+  const priorityOrderButtons = [
+    callButton,
+    textButton
+  ]
+
+  for (const button of priorityOrderButtons) {
+    if (button) {
+      button.click()
+      appLog(`Selected button clicked!`)
+      return
+    }
+  }
+}
+
+const main = (e) => {
+  appLog('Microsoft 2FA page detected!')
+  app().catch(e => {
+    appLog('Something went wrong! Reason: ', e)
+  })
+};
+
+window.addEventListener("load", main, false);
